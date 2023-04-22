0762b8bb1038b047ab5aaac5ab4f45f3e72502c4
davidcraccer <127027894+davidcraccer@users.noreply.github.com>
2023-04-22 14:35:14 +0200

    Add files via upload

--- ColorApi/index.js
+++ ColorApi/index.js
@@ -3,22 +3,50 @@ const get = id => document.querySelector(id)
 const getAll = id => document.querySelectorAll(id)
 const colorScheme = get('#color-schemes')
 const ctaBtn = get('#cta-btn')
+const hexInput = get('#color-input')
 const baseURL =  'https://www.thecolorapi.com/'
-let endpointURL = `scheme?hex=F55A5A&mode=${colorScheme.value}&count=5`
+const hexBase = '2B283A'
+let endpointURL
 
 
-setColorScheme()
+render()
+
+hexInput.addEventListener('keyup', () => {
+    render()
+})
+
+hexInput.addEventListener('input', e => {
+    limitHexInput(e)
+})
 
 ctaBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    endpointURL = `scheme?hex=F55A5A&mode=${colorScheme.value}&count=5`
-    setColorScheme()
+    render()
 })
+
+function render(){
+    const hexValue = hexInput.value.replace('#', '')
+    endpointURL = `scheme?hex=${hexValue|| hexBase}&mode=${colorScheme.value}&count=5`
+    setColorScheme()
+}
+
+
+function limitHexInput(e){
+    const hexValue = e.target.value
+    const cleanHex = hexValue.replace(/[^a-fA-F0-9]/g, '')
+    const truncatedHex = cleanHex.slice(0, 6)
+    
+    const hashtagValue = truncatedHex ? `#${truncatedHex}` : ''
+    e.target.value = hashtagValue
+}
+
+
 
 async function setColorScheme(){
     try {
         const res = await fetch(baseURL + endpointURL)
         const data = await res.json()
+
         const colors = data.colors
         const hexValues = []
         colors.forEach(color => {
@@ -33,9 +61,12 @@ async function setColorScheme(){
             colorEl.style.backgroundColor = hex
             const colorHexEl = colorsHexEl[i]
             colorHexEl.textContent = hex
+            //for ui 
+            colorHexEl.style.display = 'block'
         }
     }
     catch(err){
         console.error(err)
     }
 }
+
