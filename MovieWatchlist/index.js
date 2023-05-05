46beed1816b1a6f9d58b9c5ccaeaeaf125a4cfaa
davidcraccer <127027894+davidcraccer@users.noreply.github.com>
2023-05-05 13:03:34 +0200

    Add files via upload

--- MovieWatchlist/index.js
+++ MovieWatchlist/index.js
@@ -1,6 +1,8 @@
 const baseURL = "http://www.omdbapi.com/?apikey=a80cae51"
 const searchInput = document.querySelector('#search-input')
 
+
+
 searchInput.addEventListener('keyup', async () => {
     renderMovies()
 })
@@ -11,15 +13,30 @@ async function renderMovies(){
 
     let endpointURL = `&s=${searchInput.value}`
     const data = await getData(endpointURL)
-    data.Search.forEach(async movie => {
 
+    data.Search.forEach(async movie => {
         endpointURL = `&i=${movie.imdbID}`
         const newData = await getData(endpointURL)
+        renderMoviesHTML(newData)
+    })
+}
 
-        const { Title, Runtime, Genre, Poster, Plot, Ratings } = newData
-        const Rating = Ratings[0].Value.split("/")[0]
-      
-        document.querySelector('.hero').innerHTML +=`
+async function getData(endpointURL){
+    try {
+        const res = await fetch(baseURL + endpointURL)
+        const data = await res.json()
+        return data
+    }
+    catch (err){
+        console.error(err)
+    }
+}
+
+
+function renderMoviesHTML(data){
+    const { Title, Runtime, Genre, Poster, Plot, Ratings } = data
+    const Rating = Ratings[0].Value.split("/")[0]
+    document.querySelector('.hero').innerHTML +=`
         <div class="movie">
             <img class="poster" src="${Poster}" alt="Movie Poster">
             <div>
@@ -31,8 +48,37 @@ async function renderMovies(){
                 <div class="middle-movie">
                     <p class="small-headings">${Runtime}</p>
                     <p class="small-headings">${Genre}</p>
-                    <div class="add-btn" onclick="addToWatchList()">
+                    <div class="add-btn" onclick="addToWatchList(this)">
                         <img src="/images/plus-icon.png" alt="Add Button">
+                        <p class="small-headings">Watchlist</p>
+                    </div>
+                </div>
+                <p class="description">${renderShortDescription(Plot)}</p>
+            </div>
+        </div>
+        `
+}
+renderMyWatchlist()
+function renderMyWatchlist(){
+    let watchlist = JSON.parse(localStorage.getItem('watchlist'))
+    console.log(watchlist)
+
+    watchlist.forEach((movie, index) => {
+        const { Poster, Title, Rating, Runtime, Genre, Plot } = movie
+        document.querySelector('#hero-watchlist').innerHTML += `
+        <div class="movie">
+            <img class="poster" src="${Poster}" alt="Movie Poster">
+            <div>
+                <div class="top-movie">
+                    <p class="title">${Title}</p>
+                    <img src="/images/star.png" alt="Star">
+                    <p class="small-headings">${Rating}</p>
+                </div>
+                <div class="middle-movie">
+                    <p class="small-headings">${Runtime}</p>
+                    <p class="small-headings">${Genre}</p>
+                    <div class="remove-btn" onclick="removeFromWatchList(${index})">
+                        <img src="/images/minus-icon.png" alt="Remove Button">
                         <p class="small-headings">Watchlist</p>
                     </div>
                 </div>
@@ -42,10 +88,35 @@ async function renderMovies(){
         `
     })
 }
-async function getData(endpointURL){
-    const res = await fetch(baseURL + endpointURL)
-    const data = await res.json()
-    return data
+function removeFromWatchList(index){
+    let watchlist = JSON.parse(localStorage.getItem('watchlist'))
+    watchlist.splice(index, 1)
+    localStorage.setItem('watchlist', JSON.stringify(watchlist))
+    document.querySelector('#hero-watchlist').innerHTML = ''
+    renderMyWatchlist()
+
+}
+function addToWatchList(element){
+    const movieEl = element.closest('.movie')
+    const titleEl = movieEl.querySelector('.title')
+    const ratingEl = movieEl.querySelectorAll('.small-headings')[0]
+    const runtimeEl = movieEl.querySelectorAll('.small-headings')[1]
+    const genreEl = movieEl.querySelectorAll('.small-headings')[2]
+    const posterEl = movieEl.querySelector('.poster')
+    const plotEl = movieEl.querySelector('.description .full-description')
+
+    const movie = {
+        Title: titleEl.textContent,
+        Rating: ratingEl.textContent,
+        Runtime: runtimeEl.textContent,
+        Genre: genreEl.textContent,
+        Poster: posterEl.getAttribute('src'),
+        Plot: plotEl.dataset.fullDescription,
+    }
+
+    let watchlist = JSON.parse(localStorage.getItem('watchlist')) || []
+    watchlist.push(movie)
+    localStorage.setItem('watchlist', JSON.stringify(watchlist))
 }
 
 function renderShortDescription(Plot){
@@ -65,16 +136,4 @@ function renderFullDescription(element) {
     pElement.textContent = fullDescription;
     element.style.display = 'none'
 }
-
-
-/** 
- * http://www.omdbapi.com/?apikey=a80cae51&i=tt0083658
- * 
- * 
-(3) [{…}, {…}, {…}]
-Released
-: 
-
- */
-
 
